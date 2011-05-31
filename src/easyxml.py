5e49aaeeda766eea950e6175eb52b3731b3017d0
dainan13 <dainan13@b3508f72-e552-11de-8ee0-a52971d33838>
2011-05-31 08:21:25 +0000

    @easyxml    fix bug of endnode with % sign

--- src/easyxml.py
+++ src/easyxml.py
@@ -84,11 +84,19 @@ def xmltrans( node, datatrans, target='XML' ):
             _rr = '</'+name+'>'
             xk, rn = ( v.strip().split('|',1) + [''] ) [:2]
             xk = xk.strip()
+            xk = False if xk == '%' else xk
             xk = None if xk == '@' else xk
             xk = int(xk.strip('$')) if xk and xk.startswith('$') else xk
             rn = datatrans.get( rn ) if rn.strip() else lambda x : x
             
             def node_trans( datas ):
+                
+                if xk is False :
+                    return sum( [ [_rl,] + \
+                                  sum([ ic[1](dict(datas.items()+[(k,xr)])) for ic in inlinechilds ],[]) + \
+                                  ['>',_rr]
+                                  for xr in rn(datas[k])
+                                ], [] )
                 
                 if xk is None :
                     return ( [_rl,] + \
@@ -173,4 +181,35 @@ if __name__ == "__main__" :
     """, datatrans={ 'add': lambda x:x+10, 'sumv': lambda x: sum(x.values()) } )
     
     print xt(A=[{'x':1,'y':2},{'x':3,'y':4}], B={'p':'hello'}, C=consts)
+    
+    data = [{u'Log-Time': u'Tue, 31 May 2011 13:31:03 CST', u'Ref_Capacity':
+93053618133399, u'File_Capacity': 93049341934167, u'Ref_Quantity':
+21835317, u'Download_Capacity': 7387156286423695, u'Object_Capacity':
+68418963032758, u'Group_Quantity': 8278, u'Upload_Quantity': 1596647,
+u'Project_Quantity': 938, u'Object_Quantity': 17007865,
+u'Download_Quantity': 10605251134, u'File_Quantity': 21834238,
+u'Partition_Used_Rate': 1, u'Partition_Quantity': 57,
+u'Upload_Capacity': 3500363201753}, {u'Log-Time': u'Tue, 31 May 2011 10:31:31 CST', u'Ref_Capacity': 93008311126702, u'File_Capacity':
+93003670775778, u'Ref_Quantity': 21817506, u'Download_Capacity':
+7383387203656727, u'Object_Capacity': 68382020015983, u'Group_Quantity':
+8275, u'Upload_Quantity': 1571721, u'Project_Quantity': 936,
+u'Object_Quantity': 16991916, u'Download_Quantity': 10593890921,
+u'File_Quantity': 21816182, u'Partition_Used_Rate': 1,
+u'Partition_Quantity': 58, u'Upload_Capacity': 3451638854023}]
+    
+    xt = buildxmltrans("""
+    !XML        !A
+    systemLog
+     log        %
+      x          Ref_Capacity
+      y          File_Capacity
+      z          Log-Time
+     xxx        
+      yyy       %
+       (a)       Log-Time
+    """)
+    
+    print xt(A=data)
+    
+    
     
