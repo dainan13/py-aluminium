a8c023f28d87c836f00493ac86968b076eca1ae8
dainan13 <dainan13@b3508f72-e552-11de-8ee0-a52971d33838>
2011-06-21 11:16:15 +0000

    add easyprotocol.py

---
+++ easyprotocol.py
@@ -0,0 +1,93 @@
+
+
+
+import re
+
+name = r'(?P<name>[a-zA-Z_]\w*)'
+length = r'\((?P<length>\s*\S+?\s*)\)'
+array = r'\[(?P<array>\s*\S+\s*)\]'
+arg = r'\{(?P<arg>\s*\S+\s*)\}'
+
+pat = '%s(%s)?(%s)?(%s)?' % (name, length, array, arg)
+
+
+class EasyBinaryProtocolError( Exception ):
+    pass
+    
+class InvalidCharactorFound( EasyBinaryProtocolError ):
+    pass
+
+class ParseSyntaxError( EasyBinaryProtocolError ):
+    pass
+    
+class IndentSyntaxError( EasyBinaryProtocolError ):
+    pass
+    
+    
+
+class EasyBinaryProtocol( object ):
+    
+    def __init__( self ):
+        
+        var = r'(?P<var>[a-zA-Z_]\w*)'
+        name = r'(?P<name>[a-zA-Z_]\w*)'
+        length = r'\((?P<length>\s*\S+?\s*)\)'
+        array = r'\[(?P<array>\s*\S+\s*)\]'
+        arg = r'\{(?P<arg>\s*\S+\s*)\}'
+
+        self.pat = '%s\s+%s(%s)?(%s)?(%s)?' % (var, name, length, array, arg)
+
+    def parse( self, fname ):
+        
+        rootnode = ( None, None, [] )
+        stack = [rootnode,]
+        
+        with open(fname,'r') as fp :
+            
+            for i, li in enumerate(fp.readlines()):
+                
+                if '\t' in li :
+                    raise InvalidCharactorFound, 'ABCDEF'
+            
+                indent = len(li) - len(li.lstrip())
+            
+                li = li.strip()
+                
+                if not li :
+                    continue
+                
+                m = re.match(self.pat,li)
+                
+                if m == None :
+                    raise ParseSyntaxError, ( 'Line: %d %s' % (i,li) )
+                
+                node = ( indent, m.groupdict(), [] )
+                
+                if indent > stack[-1][0] :
+                    
+                    stack[-1][2].append( node )
+                    stack.append( node )
+                    
+                    continue
+                
+                while( stack[-1][0] > indent ):
+                    stack.pop()
+                
+                if indent != stack[-1][0] :
+                    raise IndentSyntaxError, ( 'Line: %d %s %d %d' % (i,li) )
+                
+                stack.pop()
+                
+                stack[-1][2].append(node)
+                stack.append(node)
+        
+        return rootnode
+        
+ebp = EasyBinaryProtocol()
+
+if __name__ == '__main__' :
+    
+    import pprint
+    
+    pprint.pprint( ebp.parse( 'replication.protocol' ) )
+    
