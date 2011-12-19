71d11ce7784acb84c17f6a76414808ac6c98bdfd
dainan13 <dainan13@b3508f72-e552-11de-8ee0-a52971d33838>
2011-12-19 11:07:27 +0000

    update easyprotocol2.py

--- easyprotocol2.py
+++ easyprotocol2.py
@@ -1,5 +1,17 @@
 
-from pyparsing import Word, alphas, nums, alphanums, delimitedList, Optional, Forward, StringEnd, Keyword, Suppress
+from pyparsing import Word, alphas, nums, alphanums, delimitedList, \
+Optional, Forward, StringEnd, Keyword, Suppress, Literal, CaselessLiteral, \
+Combine, ZeroOrMore
+
+exprStack = []
+varStack  = []
+
+def pushFirst( str, loc, toks ):
+    exprStack.append( toks[0] )
+
+def assignVar( str, loc, toks ):
+    varStack.append( toks[0] )
+    
 
 _keywords = [ 'struct', 'union', 
               'true', 'false', 'none', 'auto',  
@@ -21,12 +33,61 @@ expr = Forward()
 call = ident + LPAREN + delimitedList(expr) + RPAREN
 etok = ( call | ident | numvalue ) + oper + expr
 expr << ( etok | call | ident | numvalue )
+
+name = ident.copy().setName('name')
+struct = ident.copy().setName('struct')
+length = Optional( LPAREN+expr+RPAREN ).setName('length')
+array = Optional( LBRACK+expr+RBRACK ).setName('array')
+
+#syntax = Optional( cases + ':' ) \
+#         + ident.setName('name') + ident.setName('struct') \
+#         + Optional( LPAREN+expr+RPAREN ).setName('length') \
+#         + Optional( LBRACK+expr+RBRACK ).setName('array') \
+#         + StringEnd()
+
 syntax = Optional( cases + ':' ) \
-         + ident.setName('name') + ident.setName('struct') \
-         + Optional( LPAREN+expr+RPAREN ).setName('length') \
-         + Optional( LBRACK+expr+RBRACK ).setName('array') \
+         + name + struct \
+         + length \
+         + array \
          + StringEnd()
 
 #print expr.parseString( 'a(1,1)+1' )
 x = syntax.parseString( "true:name struct(abc)[a(1,1)+b(2,2)]" )
 print x
+print x.asDict()
+
+    
+point = Literal('.')
+e = CaselessLiteral('E')
+plusorminus = Literal('+') | Literal('-')
+number = Word(nums) 
+integer = Combine( Optional(plusorminus) + number )
+floatnumber = Combine( integer +
+                       Optional( point + Optional(number) ) +
+                       Optional( e + integer )
+                     )
+
+plus  = Literal( "+" )
+minus = Literal( "-" )
+mult  = Literal( "*" )
+div   = Literal( "/" )
+lpar  = Literal( "(" ).suppress()
+rpar  = Literal( ")" ).suppress()
+addop  = plus | minus
+multop = mult | div
+expop = Literal( "^" )
+assign = Literal( "=" )
+
+expr = Forward()
+atom = ( ( e | floatnumber | integer | ident ).setParseAction(pushFirst) | 
+         ( lpar + expr.suppress() + rpar )
+       )
+        
+factor = Forward()
+factor << atom + ZeroOrMore( ( expop + factor ).setParseAction( pushFirst ) )
+        
+term = factor + ZeroOrMore( ( multop + factor ).setParseAction( pushFirst ) )
+expr << term + ZeroOrMore( ( addop + term ).setParseAction( pushFirst ) )
+
+print expr.parseString('a+(1+2)*5+7')
+print exprStack
