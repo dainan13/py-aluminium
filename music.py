50c609fe87894c3c6c03e744b7473ccaaf618521
dainan13 <dainan13@b3508f72-e552-11de-8ee0-a52971d33838>
2012-01-17 11:16:49 +0000

    update

--- music.py
+++ music.py
@@ -2,18 +2,40 @@
 from pyquery import PyQuery as pq
 import urllib
 
-d = pq('http://www.kamiro.net/diary/info_2761.shtml')
-playlists = d("embed")
-playlists = [ pq(plist).attr("src") for plist in playlists ]
-print playlists
-playlists = [ urllib.urlopen(plist).geturl() for plist in playlists ]
-playlists = [ plist.split('?',1)[1] for plist in playlists ]
-playlists = [ dict( item.split('=',1) for item in plist.split('&') ) for plist in playlists ]
-playlists = [ plist['xml'] for plist in playlists ]
-playlists = [ urllib.unquote(plist) for plist in playlists ]
-print playlists
 
+def get_playlists( pageurl ):
+    
+    d = pq( pageurl )
+    playlists = d("embed")
+    playlists = [ pq(plist).attr("src") for plist in playlists ]
+    playlists = [ urllib.urlopen(plist).geturl() for plist in playlists ]
+    playlists = [ plist.split('?',1)[1] for plist in playlists ]
+    playlists = [ dict( item.split('=',1) for item in plist.split('&') ) for plist in playlists ]
+    playlists = [ plist['xml'] for plist in playlists ]
+    playlists = [ urllib.unquote(plist) for plist in playlists ]
+    
+    return playlists
 
-p = pq('<p class="hello">Hi</p><p>Bye</p>')('p')
-#print p.attr.id
-print p
+def get_tracks( playlist ):
+    
+    #d = pq( playlist, parser='html_fragments' )
+    c = urllib.urlopen(playlist).read()
+    #c = urllib.urlopen(playlist).read().replace('<![CDATA[','').replace(']]>','')
+    #print c
+    d = pq( c, parser='html_fragments' )
+    
+    titles = d('title')
+    print len(titles)
+    titles = [ t.text_content() for t in titles ]
+    
+    tracks = d('location')
+    tracks = [ t.text_content() for t in tracks ]
+    
+    return titles[0], zip(titles[1:],tracks)
+
+if __name__ == '__main__' :
+    
+    playlists = get_playlists('http://www.kamiro.net/diary/info_2761.shtml')
+    
+    for plist in playlists :
+        print get_tracks(plist)
