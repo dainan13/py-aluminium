9ff531ef87cfd1141c843ec5c87275dc80892032
dainan13 <dainan13@b3508f72-e552-11de-8ee0-a52971d33838>
2012-02-06 12:03:30 +0000

    update postscript.py code

--- postscript.py
+++ postscript.py
@@ -70,485 +70,280 @@ index       -
 roll        -
 '''
 
-#operators      argc rstc
+#operators      argc
 psoperators = r'''
-clip            0    0
-closepath       0    0
-concat          1    0
-curveto         6    0
-eoclip          0    0
-eofill          0    0
-fill            0    0
-grestore        0    0    
-gsave           0    0
-lineto          2    0
-moveto          2    0
-selectfont      1    0
-setcachedevice  6    0
-setcharwidth    2    0
-setcmykcolor    4    0 
-setcolor
-setcolorspace   1    0
-setdash         2    0
-setflat         1    0
-setgray         1    0 
-setlinecap      1    0
-setlinejoin     1    0
-setlinewidth    1    0
-setmiterlimit   1    0
-setrgbcolor     3    0
-shfill          1    0
-show            1    0
-stroke          0    0
+clip            0    
+closepath       0    
+concat          1    
+curveto         6    
+eoclip          0    
+eofill          0    
+fill            0    
+grestore        0        
+gsave           0    
+lineto          2    
+moveto          2    
+selectfont      2    
+setcachedevice  6    
+setcharwidth    2    
+setcmykcolor    4     
+setcolor        (n)
+setcolorspace   1    
+setdash         2    
+setflat         1    
+setgray         1     
+setlinecap      1    
+setlinejoin     1    
+setlinewidth    1    
+setmiterlimit   1    
+setrgbcolor     3    
+shfill          1    
+show            1    
+stroke          0    
 '''
 
-pdfoperators = r'''
-b
-closepath, fill, stroke
-Close, fill, and stroke path using nonzero winding number rule
-4.10
-230
-
-B
-fill, stroke
-Fill and stroke path using nonzero winding number rule
-4.10
-230
-
-b*
-closepath, eofill, stroke
-Close, fill, and stroke path using even-odd rule
-4.10
-230
-
-B*
-eofill, stroke
-Fill and stroke path using even-odd rule
-4.10
-230
-
-BDC
-(PDF 1.2) Begin marked-content sequence with property list
-10.7
-851
-
-BI
-Begin inline image object
-4.42
-352
-
-BMC
-(PDF 1.2) Begin marked-content sequence
-10.7
-851
-
-BT
-Begin text object
-5.4
-405
-
-BX
-(PDF 1.1) Begin compatibility section
-3.29
-152
-
-c
-curveto
-Append curved segment to path (three control points)
-4.9
-226
-
-cm
-concat
-Concatenate matrix to current transformation matrix
-4.7
-219
-
-CS
-setcolorspace
-(PDF 1.1) Set color space for stroking operations
-4.24
-287
-
-cs
-setcolorspace
-(PDF 1.1) Set color space for nonstroking operations
-4.24
-287
-
-d
-setdash
-Set line dash pattern
-4.7
-219
-
-d0
-setcharwidth
-Set glyph width in Type 3 font
-5.10
-423
-
-d1
-setcachedevice
-Set glyph width and bounding box in Type 3 font
-5.10
-423
-
-Do
-Invoke named XObject
-4.37
-332
-
-DP
-(PDF 1.2) Define marked-content point with property list
-10.7
-851
-
-EI
-End inline image object
-4.42
-352
-
-EMC
-(PDF 1.2) End marked-content sequence
-10.7
-851
-
-ET
-End text object
-5.4
-405
-
-EX
-(PDF 1.1) End compatibility section
-3.29
-152
-
-f
-fill
-Fill path using nonzero winding number rule
-4.10
-230
-
-F
-fill
-Fill path using nonzero winding number rule (obsolete)
-4.10
-230
-
-f*
-eofill
-Fill path using even-odd rule
-4.10
-230
-
-G
-setgray
-Set gray level for stroking operations
-4.24
-288
-
-g
-setgray
-Set gray level for nonstroking operations
-4.24
-288
-
-gs
-(PDF 1.2) Set parameters from graphics state parameter dictionary
-4.7
-219
-
-h
-closepath
-Close subpath
-4.9
-227
-
-i
-setflat
-Set flatness tolerance
-4.7
-219
-
-ID
-Begin inline image data
-4.42
-352
-
-j
-setlinejoin
-Set line join style
-4.7
-219
-
-J
-setlinecap
-Set line cap style
-4.7
-219
-
-K
-setcmykcolor
-Set CMYK color for stroking operations
-4.24
-288
-
-k
-setcmykcolor
-Set CMYK color for nonstroking operations
-4.24
-288
-
-l
-lineto
-Append straight line segment to path
-4.9
-226
-
-m
-moveto
-Begin new subpath
-4.9
-226
-
-M
-setmiterlimit
-Set miter limit
-4.7
-219
-
-MP
-(PDF 1.2) Define marked-content point
-10.7
-851
-n
-End path without filling or stroking
-4.10
-230
-
-q
-gsave
-Save graphics state
-4.7
-219
-
-Q
-grestore
-Restore graphics state
-4.7
-219
-
-re
-Append rectangle to path
-4.9
-227
-
-RG
-setrgbcolor
-Set RGB color for stroking operations
-4.24
-288
-
-rg
-setrgbcolor
-Set RGB color for nonstroking operations
-4.24
-288
-
-ri
-Set color rendering intent
-4.7
-219
-
-s
-closepath, stroke
-Close and stroke path
-4.10
-230
-
-S
-stroke
-Stroke path
-4.10
-230
-
-SC
-setcolor
-(PDF 1.1) Set color for stroking operations
-4.24
-287
-
-sc
-setcolor
-(PDF 1.1) Set color for nonstroking operations
-4.24
-288
-
-SCN
-setcolor
-(PDF 1.2) Set color for stroking operations (ICCBased and special color spaces)
-4.24
-288
-
-scn
-setcolor
-(PDF 1.2) Set color for nonstroking operations (ICCBased and special color spaces)
-4.24
-288
-
-sh
-shfill
-(PDF 1.3) Paint area defined by shading pattern
-4.27
-303
-
-T*
-Move to start of next text line
-5.5
-406
-
-Tc
-Set character spacing
-5.2
-398
-
-Td
-Move text position
-5.5
-406
-
-TD
-Move text position and set leading
-5.5
-406
-
-Tf
-selectfont
-Set text font and size
-5.2
-398
-
-Tj
-show
-Show text
-5.6
-407
-
-TJ
-Show text, allowing individual glyph positioning
-5.6
-408
-
-TL
-Set text leading
-5.2
-398
-
-Tm
-Set text matrix and text line matrix
-5.5
-406
-
-Tr
-Set text rendering mode
-5.2
-398
-
-Ts
-Set text rise
-5.2
-398
-
-Tw
-Set word spacing
-5.2
-398
-
-Tz
-Set horizontal text scaling
-5.2
-398
-
-v
-curveto
-Append curved segment to path (initial point replicated)
-4.9
-226
-
-w
-setlinewidth
-Set line width
-4.7
-219
-
-W
-clip
-Set clipping path using nonzero winding number rule
-4.11
-235
-
-W*
-eoclip
-Set clipping path using even-odd rule
-4.11
-235
-
-y
-curveto
-Append curved segment to path (final point replicated)
-4.9
-226
-
-'
-Move to next line and show text
-5.6
-407
-
-"
-Set word and character spacing, move to next line, and show text
-5.6
-407
+#         11    1   2         3        4      4   5
+#123456789012345678901234567890123456789012345678901234567890123456789012345678
+#operators argc ps                            description
+pdfop329 = r'''
+BX         +0   (ComSection)                  (PDF 1.1) Begin compatibility section
+EX         -0                                 (PDF 1.1) End compatibility section
 '''
+pdfop407 = r'''
+q          0    gsave                         Save graphics state
+Q          0    grestore                      Restore graphics state
+cm         6    concat                        Concatenate matrix to current transformation matrix
+w          1    setlinewidth                  Set line width
+J          1    setlinecap                    Set line cap style
+j          1    setlinejoin                   Set line join style
+M          1    setmiterlimit                 Set miter limit
+d          2    setdash                       Set line dash pattern
+ri         1    (set_color_rendering)         Set color rendering intent
+i          1    setflat                       Set flatness tolerance
+gs         1    (usegraphic)                  (PDF 1.2) Set parameters from graphics state parameter dictionary
+'''
+pdfop409 = r'''
+m          2    moveto                        Begin new subpath
+l          2    lineto                        Append straight line segment to path
+c          6    curveto                       Append curved segment to path (three control points)
+v          4    (curveto_fromcur)             Append curved segment to path (initial point replicated)
+y          4    (curveto_tocur)               Append curved segment to path (final point replicated)
+h          0    closepath                     Close subpath
+re         4    (append_rectangle)            Append rectangle to path
+'''
+pdfop410 = r'''
+S          0    stroke                        Stroke path
+s          0    closepath, stroke             Close and stroke path
+f          0    fill                          Fill path using nonzero winding number rule
+F          0    fill                          Fill path using nonzero winding number rule (obsolete)
+f*         0    eofill                        Fill path using even-odd rule
+B          0    fill, stroke                  Fill and stroke path using nonzero winding number rule
+B*         0    eofill, stroke                Fill and stroke path using even-odd rule
+b          0    closepath, fill, stroke       Close, fill, and stroke path using nonzero winding number rule
+b*         0    closepath, eofill, stroke     Close, fill, and stroke path using even-odd rule
+n          0    (endpath)                     End path without filling or stroking
+'''
+pdfop411 = r'''
+W          0    clip                          Set clipping path using nonzero winding number rule
+W*         0    eoclip                        Set clipping path using even-odd rule
+'''
+pdfop424 = r'''
+CS         1    setcolorspace                 (PDF 1.1) Set color space for stroking operations
+cs         1    setcolorspace                 (PDF 1.1) Set color space for nonstroking operations
+SC         n    setcolor                      (PDF 1.1) Set color for stroking operations
+SCN        n    setcolor                      (PDF 1.2) Set color for stroking operations (ICCBased and special color spaces)
+sc         n    setcolor                      (PDF 1.1) Set color for nonstroking operations
+scn        n    setcolor                      (PDF 1.2) Set color for nonstroking operations (ICCBased and special color spaces)
+G          1    setgray                       Set gray level for stroking operations
+g          1    setgray                       Set gray level for nonstroking operations
+RG         3    setrgbcolor                   Set RGB color for stroking operations
+rg         3    setrgbcolor                   Set RGB color for nonstroking operations
+K          4    setcmykcolor                  Set CMYK color for stroking operations
+k          4    setcmykcolor                  Set CMYK color for nonstroking operations
+'''
+pdfop437 = r'''
+Do         1    (call)                        Invoke named XObject
+'''
+pdfop442 = r'''
+BI         +0   (Image)                       Begin an inline image object
+ID         +0   (Image)                       Begin the image data for an inline image object
+EI         -0                                 End an inline image object
+'''
+pdfop502 = r'''
+Tc         1    (settextspacing)              Set character spacing
+Tw         1    (setwordspacing)              Set word spacing
+Tz         1    (settext_h)                   Set horizontal text scaling
+TL         1    (settext_leading)             Set text leading
+Tf         2    selectfont                    Set text font and size
+Tr         1    (settext_rendering)           Set text rendering mode
+Ts         1    (settext_rise)                Set text rise
+'''
+pdfop504 = r'''
+BT         +0   (Text)                        Begin text object
+ET         -0                                 End text object
+'''
+pdfop505 = r'''
+Td         2    (textposition)                Move text position
+TD         2    (textposition_d)              Move text position and set leading
+Tm         6    (textmatrix)                  Set text matrix and text line matrix
+T*         0    (textpos_nextline)            Move to start of next text line
+'''
+pdfop506 = r'''
+Tj         1    show                          Begin text object
+TJ         1    (show)                        End text object
+'          1    (show_nextline)
+"          3    (show_ex)
+'''
+pdfop510 = r'''
+d0         2    setcharwidth                  Set glyph width in Type 3 font
+d1         6    setcachedevice                Set glyph width and bounding box in Type 3 font
+'''
+pdfop1007 = r'''
+MP         1    (Marked_Content)              (PDF 1.2) Define marked-content point
+DP         2    (Marked_Content)              (PDF 1.2) Define marked-content point with property list
+BMC        +1   (Marked_Content)              (PDF 1.2) Begin marked-content sequence
+BDC        +2   (Marked_Content)              (PDF 1.2) Begin marked-content sequence with property list
+EMC        -0                                 (PDF 1.2) End marked-content sequence
+'''
+#         1.    .   2         3        4      .   5
+#123456789012345678901234567890123456789012345678901234567890123456789012345678
+#operators argc ps                            description
 
-
-paser = Parser( declaration, 'EXPRESSION' )
+pdfopers = [ pdfop329, pdfop407, pdfop409, pdfop410, pdfop411, pdfop424,
+             pdfop437, pdfop442,
+             pdfop502, pdfop504, pdfop505, pdfop506, pdfop510,
+             pdfop1007,
+           ]
 
 data = 'q\nBT\n/F1 24 Tf\n24 TL\n1 0 0.2126 1 50 700 Tm\n(\x00+\x00H\x00O\x00O\x00R\x00\x03\x00Z\x00R\x00U\x00O\x00G\x00\x04\x00\x03\x04L+\x82\x05\x96\x0f\xb3\x00\x03\x00\x04)Tj\n1 0 0.2126 1 50 676 Tm\n(\x00\x0b\x00V\x00D\x00\\\\\x00V\x00\x03\x003\x00\\\\\x00W\x00K\x00R\x00Q\x00\x0c)Tj\nET\nQ\n0.5335 0.8458 -0.8458 0.5335 88.5386 11.0368 cm\n3.7611 w\nBT\n/F2 125.3695 Tf\n1 Tr\n0 TL\n0 0 Td\n(zzzPsgiolePfrp)Tj\n1 w\nET\n'
 
-pos = 0
-success = 1
-while( success ):
-    success, tree, nchar = paser.parse( data[pos:] )
-    print '----'
-    print data[pos:pos+nchar]
-    pos += nchar
-    print tree
-    print
+def initialpostscript( cls ):
+    
+    parser = Parser( declaration, 'EXPRESSION' )
+    
+    cls.parser = parser
+    
+    opers = [ l.strip() for ops in pdfopers for l in ops.splitlines() ]
+    opers = [ (l[:11].strip(), l[11:16].strip(), l[16:46].strip(), l[46:].strip()) for l in opers if l ]
 
-pdfopers = [ l.strip() for l in pdfoperators.strip().splitlines() ]
-els = [ i for i, l in enumerate(pdfopers) if l == '']
-pdfopers = [ pdfopers[i+1:j] for i,j in zip([-1]+els,els+[None]) ]
+    op_pdf2ps = [ (n,f.strip('()').split(',') ) for n, arg, f, d in opers ]
+    op_pdf2ps = [ (n, [ fi.strip() for fi in f] ) for n, f in op_pdf2ps ]
+    
+    op_cnt = [ (n, int(arg) if arg!='n' else None) for n, arg, f, d in opers ]
+    op_sec = [ (n, True if arg.startswith('+') else False if arg.startswith('-') else None ) 
+               for n, arg, f, d in opers ]    
+    
+    cls.pdfopers = dict(op_pdf2ps)
+    cls.pdfopers_argc = dict(op_cnt)
+    cls.pdfopers_sec = dict(op_sec)
+    
+    return cls
 
-psopers = [ op for op in pdfopers if len(op) == 5 ]
-psopers = [ x.strip() for op in psopers for x in op[1].strip().split(',') ]
-psopers = list(set(psopers))
-psopers.sort()
-print '\r\n'.join(psopers)
+@initialpostscript
+class PostScript(object):
+    
+    def __init__( self ):
+        self.section = []
+        
+    def decompile( self, inp ):
+        
+        pos = 0
+        success = 1
+        
+        while( success ):
+            success, tree, nchar = self.parser.parse( inp[pos:] )
+            #print '----'
+            raw = data[pos:pos+nchar]
+            pos += nchar
+            #print raw
+            #print tree
+            prefix = ' '*(4*len(self.section))
+            print prefix + (prefix+'\n').join( self._d(tree, raw, []) )
+            #print
+        
+        return
+        
+    def _d( self, inps, raw, stacks ):
+        
+        for t, s, e, subs in inps :
+            if t == 'TYPE' :
+                self._d_TYPE((t,s,e,subs), raw, stacks)
+                
+        return stacks
+        
+    def _xd_one( self, inps, raw, stacks ):
+        t, s, e, subs = inps
+        return getattr( self, '_d_'+subs[0][0] )( subs[0], raw, stacks )
+        
+    _d_TYPE = _xd_one
+    _d_NUMBERIC = _xd_one
+    _d_STRING = _xd_one
+        
+    def _d_INTEGER( self, inps, raw, stacks ):
+        t, s, e, subs = inps
+        v = int(raw[s:e])
+        stacks.append(v)
+        return v
+        
+    def _d_FLOAT( self, inps, raw, stacks ):
+        t, s, e, subs = inps
+        v = float(raw[s:e])
+        stacks.append(v)
+        return v
+        
+    def _d_OPERATOR( self, inps, raw, stacks ):
+        
+        t, s, e, subs = inps
+        f = raw[s:e]
+        
+        if self.pdfopers_sec[f] == False :
+            self.section.pop(-1)
+            return
+        
+        ps_opers = self.pdfopers[f]
+        argc = self.pdfopers_argc[f]
+        argclist = [0]*(len(ps_opers)-1) + [argc]
+        
+        for oper, argc in zip(ps_opers, argclist):
+            if argc == 'n' :
+                raise Exception, 'n not supported yet'
+            args = [ str(stacks.pop(-1)) for i in range(argc) ]
+            args.reverse()
+            v = oper + '(' + ', '.join(args) + ')'
+            stacks.append(v)
+            
+        if self.pdfopers_sec[f] == True :
+            self.section.append(v)
+            stacks.append( 'with '+stacks.pop(-1)+' :' )
+            
+        return v
+        
+    def _d_LITERAL_STRING( self, inps, raw, stacks ):
+        t, s, e, subs = inps
+        v = raw[s+1:e-1]
+        v = repr(v)
+        stacks.append(v)
+        return v
+        
+    def _d_HEXADECIMAL_STRING( self, inps, raw, stacks ):
+        t, s, e, subs = inps
+        v = raw[s+1:e-1]
+        v = repr(v)
+        stacks.append(v)
+        return v
+        
+    def _d_NAME( self, inps, raw, stacks ):
+        t, s, e, subs = inps
+        v = raw[s+1:e]
+        stacks.append(v)
+        return v
+        
+    def compile( self ):
+        pass
 
+ps = PostScript()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__' :
+    
+    ps.decompile(data)
+    
