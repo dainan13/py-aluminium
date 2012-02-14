695bb2c642fa95c2cd9a0a57f3afbe33068f492f
dainan13 <dainan13@b3508f72-e552-11de-8ee0-a52971d33838>
2012-02-14 10:49:30 +0000

    add a lib for textwrap ;

---
+++ textwrap.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+initials = u'([{<$'
+           u'￥【“‘《'
+           
+tails = u'!%)]:;>?,.'
+        u'·！…）】、：；”’》，。？'
+           
+letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+         'abcdefghijklmnopqrstuvwxyz'
+         '0123456789'
+         '~`@#^&*_-+=|\\"\'/'
+
+def split_word( s ):    
+    
+    r = []
+    
+    onword = True
+    innerspace = False
+    wordpos = 0
+    spacepos = 0
+    
+    for i, c in enumerate(s) :
+        
+        if onword :
+            
+            if c in letter or c in tails :
+                innerspace = False
+                continue
+            elif c in initials :
+                innerspace = True
+                continue
+            elif c == ' ' :
+                spacepos = i
+                if not innerspace :
+                    onword = False
+                continue
+            elif c in '\t\r\n' :
+                spacepos = i
+                r.append( (wordpos, spacepos) )
+                wordpos, spacepos = i, i
+                innerspace = False
+            else : # for chnchr
+                wordpos, spacepos = i+1, i+1
+                r.append( (wordpos, spacepos) )
+                innerspace = False
+                onword = False
+                continue
+        else :
+            
+            if c in letter :
+                r.append( (wordpos, spacepos) )
+                wordpos, spacepos = i, i
+                onword = True
+                innerspace = False
+                continue
+                
+            elif c in initials :
+                r.append( (wordpos, spacepos) )
+                wordpos, spacepos = i, i
+                onword = True
+                innerspace = True
+                continue
+                
+            elif c in tails :
+                onword = True
+                spacepos = wordpos
+                innerspace = False
+                continue
+                
+            elif c in '\t\r\n' :
+                spacepos = i
+                r.append( (wordpos, spacepos) )
+                wordpos, spacepos = i, i
+                innerspace = False
+                continue
+                
+            else :
+                wordpos, spacepos = i+1, i+1
+                r.append( (wordpos, spacepos) )
+                innerspace = False
+                onword = False
+                continue
+                
+    r.append( (wordpos, spacepos) )
+    
+    return r
+    
+    
