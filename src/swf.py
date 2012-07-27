c2f641c4553f8792b5030d6630d06183aeecb8ec
dainan13 <dainan13@b3508f72-e552-11de-8ee0-a52971d33838>
2012-07-27 11:46:11 +0000

    @swf     update

--- src/swf.py
+++ src/swf.py
@@ -112,9 +112,8 @@ class Rect( ezp.ProtocolType ):
     def read( self, namespace, fp, lens, args ):
         
         c = fp.read(1)
-        nbits = c >> 3
-        
-        c = c & 7
+        ci = ord(c)
+        nbits = ci >> 3
         
         nexttoread = ( nbits*4 + 5 + 7 ) / 8 - 1 
         
@@ -131,6 +130,7 @@ class ShockwaveFile(object):
     
     ebp = ezp.EasyBinaryProtocol()
     ebp.buildintypes.append(TagCodeAndLenght())
+    ebp.buildintypes.append(Rect())
     ebp.buildintypes.append(String())
     ebp.rebuild_namespaces()
     ebp.parsefile( __filepath__+'/protocols/swf.protocol' )
@@ -163,7 +163,7 @@ class ShockwaveFile(object):
                 self.fonts[font['fontname']] = font
                 
                 
-        pprint.pprint( self.fonts )
+        #pprint.pprint( self.fonts )
     
     def dump_swf( self, fname ):
         
@@ -172,9 +172,12 @@ class ShockwaveFile(object):
             1 + 1 + 2 + \
             sum( self._len_tag(t) for t in self.tags )
         
-        with open( fname ) as fp :
+        with open( fname, 'wb' ) as fp :
             
             self._dump_header( fp )
+            
+            for tag in self.tags :
+                self._dump_tag( fp, tag )
         
         return
         
@@ -199,31 +202,158 @@ class ShockwaveFile(object):
         
     def _dump_tag( self, fp, tag ):
         
-        fp.write( int16( (tag['code'] << 6) + 63 ) )
-        fp.write( int32( tag['length'] ) )
+        fp.write( int16( (tag['code_and_length']['code'] << 6) + 63 ) )
         
         if tag['code_and_length']['code'] == 91 :
             return self._dump_tag_91( fp, tag )
             
+        tag['code_and_length']['length'] = len(tag['content'])
+        fp.write( int32( tag['code_and_length']['length'] ) )
         fp.write( tag['content'] )
         
         return
         
     def _dump_tag_91( self, fp, tag ):
         
+        le = 2 + 1 + len(tag['content']['fontname']) + 1 + len(tag['content']['fontdata'])
+        tag['code_and_length']['length'] = le
+        fp.write( int32( tag['code_and_length']['length'] ) )
+        
         fp.write( int16(tag['content']['fontID']) )
         fp.write( int8(tag['content']['fontflag']) )
-        fp.write( int8(tag['content']['fontname']) )
+        fp.write( tag['content']['fontname'] )
         fp.write( '\0' )
         fp.write( tag['content']['fontdata'] )
         
         return
     
+    def print_tag( self, tag ):
+        
+        print '= TAG ='
+        print 'CODE', tag['code_and_length']['code']
+        print 'LENGTH', tag['code_and_length']['length']
+        
+        if tag['code_and_length']['code'] == 91 :
+            print '  fontID', tag['content']['fontID']
+            print '  fontname',tag['content']['fontname']
+            return 
+        
+        print tag['content'].encode('string_escape')
+        
+        return
     
 if __name__ == '__main__' :
     
     import sys
+    import getopt
+    
+    opts, args = getopt.gnu_getopt(
+        sys.argv[1:],
+        'o:ih',
+        ['output=', 'info', 'font=', 'help']
+    )
+    
+    helpinfo = '''\
+SWF tool. present by py-Al project. it written by python.
+
+
+examples :
+
+   swf.py [--font xxx:yyy] [-o outputfile] <xxx.swf>
+ or 
+   swf.py <xxx.swf> --info
+
+command arguments :
+
+ common :
+    --output      -o file      : output file
+    
+ for display info :
+    --info        -i           : show file infomations
+ 
+ for embedded font :
+    --font           name:font : embed font
+ 
+ for help :
+    --help        -h           : to show this help info.
+'''
+    
+    if len(sys.argv) < 2 :
+        print helpinfo
+        sys.exit(-1)
+        
     
     swf = ShockwaveFile( sys.argv[1] )
+
+    output = None
+    info = False
+    
+    for k, v in opts :
+        
+        k = k.strip('-')
+        
+        if k in ('font',) :
+            name, fontfile = v.split(':',1)
+            with open(fontfile, 'rb') as fp :
+                swf.fonts[name]['fontdata'] = fp.read()
+                
+        elif k in ('output','o') :
+            output = v
+            
+        elif k in ('info', 'i'):
+            info = True
+            
+    if output != None :
+        
+        swf.dump_swf( output )
+        
+    if info :
+        
+        print 'SWF INFO:'
+        print 
+        print swf.header
+        
+        for tag in swf.tags :
+            swf.print_tag(tag)
+            print
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
